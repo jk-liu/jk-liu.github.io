ce5dc0e2643c3fa8e6b3cdbdad62b3e485127dd0
JK Liu <liu.jay@outlook.com>
2015-03-08 03:53:44 -0400

    added spinners and stop name lookup

--- rtbus/rtbus.js
+++ rtbus/rtbus.js
@@ -1,5 +1,6 @@
 var baseUrl = "https://jsonp.nodejitsu.com/?callback=&url=http%3A%2F%2Frealtimemap.grt.ca%2FStop%2FGetStopInfo%3FstopId%3D";
 var routeUrl = "%26routeId%3D";
+var spinnerHtml = "<i class=\"fa fa-refresh fa-spin fa-2x\"></i>";
 
 function main() {
     $('#disp1').slideUp();
@@ -21,7 +22,9 @@ function main() {
     if (d.getHours() < 12) {
         $('#disp1').slideDown();
         $('#disp2').slideDown();
-
+		$('#conestoga202boardwalk').slideDown();
+		$('#laurier200ainsle').slideDown();
+		
         sendJSONtoDiv(2832, 202, "conestoga202boardwalk");
         sendJSONtoDiv(3620, 200, "laurier200ainsle");
     }
@@ -29,7 +32,9 @@ function main() {
     else {
         $('#disp3').slideDown();
         $('#disp4').slideDown();
-
+		$('#victoria200conestoga').slideDown();
+		$('#laurier202conestoga').slideDown();
+		
         sendJSONtoDiv(1893, 200, "victoria200conestoga");
         sendJSONtoDiv(3620, 202, "laurier202conestoga");
     }
@@ -40,15 +45,27 @@ function customInfo() {
     var routeId = $("#inputRouteId").val();
 
     $('#customBusTitle').slideUp();
-    $('#customBusInfo').slideUp();
+    $('#customBusTitle').html("<h4>Route " + routeId + "</h4><h5>Stop #" + stopId + "</h5>");
+    $('#customBusTitle').slideDown();
 
+    $('#customBusInfo').slideUp();
+    $('#customBusInfo').html(spinnerHtml);
+    $('#customBusInfo').slideDown();
+
+	var stopName = "";
+	
+	$.getJSON("stops.json", function (data) {
+		stopName = data[(stopId-1000)];
+	});
+	
     $.getJSON(baseUrl + stopId + routeUrl + routeId, function (data) {
         var customBusTitle = "<h4>" + data.stopTimes[0].HeadSign + "</h4>";
-        customBusTitle += "<h5> Stop #" + stopId + "</h5>";
+        customBusTitle += "<h5>" + stopName + " - Stop #" + stopId + "</h5>";
 
+        $('#customBusTitle').slideUp();
+        $('#customBusTitle').html(customBusTitle);
         $('#customBusTitle').slideDown();
 
-        document.getElementById("customBusTitle").innerHTML = customBusTitle;
         setDivs(data.stopTimes, "customBusInfo");
     });
 
@@ -82,6 +99,7 @@ function setDivs(data, divID) {
  
     outputString += "</table>";
  
+	$("#"+divID).slideUp();
     document.getElementById(divID).innerHTML = outputString;
     $("#"+divID).slideDown();
 }
